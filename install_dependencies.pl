d2f77b2ee25a686208569943b41d9d2d664ee0ee
Niles Wyles <4951296+nileswyles@users.noreply.github.com>
2024-11-06 17:22:34 -0500

    Update install_dependencies.pl

--- install_dependencies.pl
+++ install_dependencies.pl
@@ -42,3 +42,8 @@ system("apt install -y zip");
 system("apt install -y unzip");
 system("apt install -y tar");
 system("apt install -y pkg-config");
+
+# gcloud CLI
+system("curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg");
+system("echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list");
+system("apt-get update && sudo apt-get install google-cloud-cli");
