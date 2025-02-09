141394ae358fe671017d10a1d74346025a109457
flux capacitor <fluxcapacitor@LOL.com>
2025-02-09 13:20:25 -0500

    temp fix for linux_goodness scripts install

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM --platform=linux/amd64 mcr.microsoft.com/devcontainers/base:jammy
 
-COPY --chmod=700 linux_goodness/scripts/perl_modules /scripts/perl_modules
+# TODO: until mounting works.
+COPY --chmod=700 linux_goodness/scripts /scripts
 COPY --chmod=700 install_dependencies.pl /install_dependencies.pl
 COPY --chmod=700 lame.pl /lame.pl
-# TODO: until mounting works.
-COPY --chmod=700 linux_goodness /linux_goodness
 RUN apt update
 RUN apt install perl -y
 # RUN --mount=type=bind,src=/workspace,target=../ .devcontainer/install_dependencies.pl
--- install_dependencies.pl
+++ install_dependencies.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -I/scripts/perl_modules
-# convention is to install to the root folder.
+# convention is to install linux_goodness/scripts to the root folder.
 
 use File;
 # assumes ubuntu jammy version-crucial dependencies
@@ -40,6 +40,4 @@ system("curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearm
 system("echo \"deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main\" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list");
 system("apt-get update && sudo apt-get install -y google-cloud-cli");
 
-# convention is to install to root directory
-system("ln -sf $abs_path_to_containing_folder/linux_goodness/scripts /scripts");
 system("/scripts/cpp-reflection/install.pl");
